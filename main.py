b87b03a769107e6a3561e6c9e0b8f72d815a1357
Cvetan <cvetanveljanovski@gmail.com>
2021-07-18 19:20:45 +0200

    Testing a live change in a file

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! CD'
+    return 'Hello Cvetan! '
 
 @app.route('/echo/<name>')
 def echo(name):
